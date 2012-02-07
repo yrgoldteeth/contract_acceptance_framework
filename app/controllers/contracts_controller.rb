f1b0feb52d34bba2945196f6e6d9a76cca360bd0
Nicholas Fine <nicholas.fine@gmail.com>
2012-02-07 13:58:41 -0600

    update controller

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,10 +1,8 @@
 module ContractAcceptanceFramework
   class ::ContractsController < ApplicationController
-    respond_to :json
 
     def index
       @contracts = ContractAcceptanceFramework::Contract.to_adapter.find_all(:order => 'key')
-      respond_with @contracts
     end
   end
 end
